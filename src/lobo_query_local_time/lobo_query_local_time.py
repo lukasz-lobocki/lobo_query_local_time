d5876fc1da50dc2557d622448b770b8f1ef6a530
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-08-05 10:30:46 +0200

    feat: Adding subseconds, refactor to regex.

--- src/lobo_query_local_time/lobo_query_local_time.py
+++ src/lobo_query_local_time/lobo_query_local_time.py
@@ -1,3 +1,5 @@
+import re
+
 import ujson as json
 import urequests as requests
 
@@ -5,7 +7,7 @@ DEBUG = False
 _api_url = "http://worldtimeapi.org/api/ip"
 
 
-def _query_local_timestring(api_url: str) -> tuple[str, str]:
+def _query_local_timestring(api_url: str) -> tuple[str | None, str | None]:
     try:
         _response = requests.get(api_url)
         _data = json.loads(_response.text)
@@ -16,38 +18,47 @@ def _query_local_timestring(api_url: str) -> tuple[str, str]:
             return _datetime, _day_of_week
         else:
             DEBUG and print("Error:", _data.get("error", "Unknown error"))
-            return None
+            return None, None
     except Exception as e:
         DEBUG and print("An error occurred:", e)
-        return None
+        return None, None
 
 
-def get_local_datetime():
-    _local_timestring, _day_of_week = _query_local_timestring(_api_url)
+def get_local_datetime(api_url: str = _api_url) -> tuple[int, int, int, int, int, int, int, int]:
+    # Returns datetime tuple (year, month, day, weekday {Mon=0}, hours, minutes, seconds, subseconds)
+    _local_timestring, _day_of_week = _query_local_timestring(api_url)
 
     if _local_timestring:
-        _date_part, _time_part = _local_timestring.split("T")
-        _time_part, _ = _time_part.split("+")
-        DEBUG and print(
-            "_local_timestring {ts}; "
-            "_date_part {dp}, "
-            "_time_part {tp}".format(
-                ts=_local_timestring,
-                dp=_date_part,
-                tp=_time_part))
+        # Split ISO time by those characters.
+        _regex = re.compile("[-T:.+]")
+        _split = _regex.split(_local_timestring)
 
-        date_components = _date_part.split("-")
-        time_components = _time_part.split(":")
+        _year = int(_split[0])
+        _month = int(_split[1])
+        _day = int(_split[2])
 
-        year = int(date_components[0])
-        month = int(date_components[1])
-        day = int(date_components[2])
+        _day_of_week = int(_day_of_week)
 
-        hour = int(time_components[0])
-        minute = int(time_components[1])
-        second = int(float(time_components[2]))
+        _hour = int(_split[3])
+        _minute = int(_split[4])
+        _second = int(_split[5])
+        _subsecond = int(_split[6])
+    else:
+        # Start of epoch
+        _year = 2000
+        _month = 1
+        _day = 1
 
-        return year, month, day, (_day_of_week + 6) % 7, hour, minute, second, 0
+        # It was Saturday
+        _day_of_week = 6
+
+        _hour = 0
+        _minute = 0
+        _second = 0
+        _subsecond = 0
+
+    # Recalculate weekday from Sunday = 0 to Monday = 0 notation.
+    return _year, _month, _day, (_day_of_week + 6) % 7, _hour, _minute, _second, _subsecond
 
 
 if __name__ == "__main__":
